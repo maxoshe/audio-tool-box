d4b0c2a9f24a6bdc79213e96ad43b90f1a8d9e91
moshes <maxoshe@gmail.com>
2025-08-28 11:50:28 -0700

    added channel tests

---
+++ tests/test_channel.py
@@ -0,0 +1,28 @@
+from audio_tool_box.audio_data import AudioData
+from audio_tool_box.channel import Channel
+from pathlib import Path
+
+
+def test_channel_read_write(tmp_path: Path, test_tone: AudioData):
+    test_channel = Channel(source=test_tone)
+    assert test_channel.audio_data.get_duration_s() > 0
+
+    file_path = tmp_path / "test.wav"
+    test_channel.write(file_path)
+
+    test_channel = Channel(source=file_path)
+    assert test_channel.audio_data.get_duration_s() > 0
+
+
+def test_channel_processing(test_tone: AudioData):
+    test_channel = Channel(source=test_tone)
+    test_channel.compressor()
+    test_channel.eq_band()
+    test_channel.fade()
+    test_channel.gain()
+    test_channel.highpass()
+    test_channel.limiter()
+    test_channel.lowpass()
+    test_channel.noise_reduction()
+    test_channel.normalize()
+    test_channel.soft_clipping()
