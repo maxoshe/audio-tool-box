bd57aa89ad2b92294466f7fa91c168ee5ae10217
moshes <maxoshe@gmail.com>
2025-08-31 12:45:40 -0700

    remove duplicate example

--- examples/filter_signal.py
+++
@@ -1,8 +0,0 @@
-from audio_toolset.channel import Channel
-
-track = Channel("my_audio.wav")
-
-track.lowpass(cutoff_frequency=12000)  # Remove harsh high frequencies
-track.highpass(cutoff_frequency=80)  # Remove low-end rumble
-
-track.write("audio_filtered.wav")
--- examples/quickstart.py
+++ examples/quickstart.py
@@ -2,8 +2,8 @@ from audio_toolset.channel import Channel
 
 track = Channel("my_audio.wav")
 
+track.lowpass(cutoff_frequency=12000)  # Remove harsh high frequencies
 track.highpass(cutoff_frequency=80)  # Remove low-end rumble
-track.lowpass(cutoff_frequency=12000)  # Remove harsh highs
 track.normalize(target_db=-1)  # Peak normalize
 
 track.write("my_audio_processed.wav")
